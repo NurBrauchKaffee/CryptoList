8167f36bec295d56b153db8bc4469517996df6d0
JustNeedCoffee <JustNeedCoffee@list.ru>
2024-08-20 20:02:34 +0300

    some code refactoring

--- app/src/main/java/ru/justneedcoffee/cryptolist/view/activities/MainActivity.kt
+++ app/src/main/java/ru/justneedcoffee/cryptolist/view/activities/MainActivity.kt
@@ -2,11 +2,8 @@ package ru.justneedcoffee.cryptolist.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.chip.Chip
 import ru.justneedcoffee.cryptolist.R
 
 class MainActivity : AppCompatActivity() {
